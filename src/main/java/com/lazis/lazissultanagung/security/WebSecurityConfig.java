4565b4738df1a2b8c3c7eb9f7d59fddd95c431c9
Tsabit Gholib <tsabit1502@gmail.com>
2024-09-17 23:09:33 +0700

    add oauth2 google

--- src/main/java/com/lazis/lazissultanagung/security/WebSecurityConfig.java
+++ src/main/java/com/lazis/lazissultanagung/security/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -61,6 +62,11 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
                         auth.anyRequest().permitAll()
+                )
+                .oauth2Login(oauth2Login ->
+                        oauth2Login
+                                .userInfoEndpoint()
+                                .oidcUserService(oidcUserService())
                 );
 
         http.authenticationProvider(authenticationProvider());
@@ -68,5 +74,10 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    public OidcUserService oidcUserService() {
+        return new OidcUserService();
     }
 }
