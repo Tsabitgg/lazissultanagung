16877e5822bb0217f3f765f0c5b7587a7c6d71ea
Tsabit Gholib <tsabit1502@gmail.com>
2024-10-22 21:22:27 +0700

    add jurnal

--- src/main/java/com/lazis/lazissultanagung/controller/JurnalController.java
+++ src/main/java/com/lazis/lazissultanagung/controller/JurnalController.java
@@ -6,14 +6,12 @@ import com.lazis.lazissultanagung.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@CrossOrigin(origins= {"*"}, maxAge = 4800, allowCredentials = "false" )
 @RestController
 @RequestMapping("/api/journal")
 public class JurnalController {
